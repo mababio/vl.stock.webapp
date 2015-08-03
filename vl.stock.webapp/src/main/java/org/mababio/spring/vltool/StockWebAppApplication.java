c3cc412f7f06dc608f4cdf127f5e8736ce57c5dc
Michael Ababio <michaelkwasi@gmail.com>
2015-08-02 23:25:50 -0400

    First commit

---
+++ vl.stock.webapp/src/main/java/org/mababio/spring/vltool/StockWebAppApplication.java
@@ -0,0 +1,12 @@
+package org.mababio.spring.vltool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockWebAppApplication.class, args);
+    }
+}
